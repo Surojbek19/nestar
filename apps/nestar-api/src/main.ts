deb3a20e4bbe9d68548faa860ee2b3386367c88c
Sam <surojbeksv@gmail.com>
2025-12-31 15:06:40 +0900

    feat: develop logging via global interceptors

--- apps/nestar-api/src/main.ts
+++ apps/nestar-api/src/main.ts
@@ -10,3 +10,37 @@ async function bootstrap() {
   await app.listen(process.env.PORT_API ?? 3000);
 }
 bootstrap();
+
+/**
+ * 
+ import { Injectable, NestInterceptor, ExecutionContext, CallHandler, Logger } from '@nestjs/common';
+ import { GqlContextType } from '@nestjs/graphql';
+ import { Observable } from 'rxjs';
+ import { tap } from 'rxjs/operators';
+ 
+ @Injectable()
+ export class LoggingInterceptor implements NestInterceptor {
+     private readonly logger: Logger = new Logger();
+ 
+ 
+   intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
+       const recordTime = Date.now();
+       const requestType = context.getType<GqlContextType>();
+ 
+       if(requestType === 'http') {
+         // Develop id needed!
+       } else if(requestType === 'graphql') {
+         this.logger.log(`Type: ${requestType}`, 'REQUEST');
+         return next
+         .handle()
+         .pipe(
+             tap(() => {
+                 const responseTime = Date.now() - recordTime
+                 this.logger.log(`${responseTime}ms`, 'RESPONSE')
+             }),
+         );
+       }
+   }
+ }
+ 
+ */
