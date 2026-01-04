a64412ffc36aad78e46928b87ef12d1d00996ef6
Sam <surojbeksv@gmail.com>
2026-01-05 03:05:16 +0900

    feat: develop single and multi image uploadeer mutation GraphQl APIs

--- apps/nestar-api/src/libs/config.ts
+++ apps/nestar-api/src/libs/config.ts
@@ -2,6 +2,18 @@ import { ObjectId } from "bson";
 
 export const availableAgentSorts = ["createdAt", "updatedAt", "memberLikes", "memberViews", "memberRank" ]
 
+
+/** IMAGE CONFIGURATION **/ 
+import { v4 as uuidv4 } from 'uuid';
+import * as path from 'path';
+
+export const validMimeTypes = ['image/png', 'image/jpg', 'image/jpeg'];
+export const getSerialForImage = (filename: string) => {
+	const ext = path.parse(filename).ext;
+	return uuidv4() + ext;
+};
+
+
 export const availableMemberSorts = ["createdAt", "updatedAt", "memberLikes", "memberViews" ]
 
 export const shapeIntoMongoObjectId = (tager: any) => {
