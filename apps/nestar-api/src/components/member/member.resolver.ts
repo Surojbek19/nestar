67c1f8568b58ca94c5ecd44cd5d157e3bb14073a
Sam <surojbeksv@gmail.com>
2026-01-04 22:43:45 +0900

    feat: develop getAllMembersByAdmin and updateMemberByAdmin

--- apps/nestar-api/src/components/member/member.resolver.ts
+++ apps/nestar-api/src/components/member/member.resolver.ts
@@ -1,6 +1,6 @@
 import { Mutation, Resolver, Query, Args } from '@nestjs/graphql';
 import { MemberService } from './member.service';
-import { AgentInquery, LoginInput, MemberInput } from '../../libs/dto/member/member.input';
+import { AgentInquery, LoginInput, MemberInput, MembersInquery } from '../../libs/dto/member/member.input';
 import { Member, Members } from '../../libs/dto/member/member';
 import { UseGuards } from '@nestjs/common';
 import { AuthGuard } from '../auth/guards/auth.guard';
@@ -20,13 +20,13 @@ export class MemberResolver {
         @Mutation(() => Member)
         public async signup(@Args("input") input: MemberInput): Promise<Member> {
                 console.log("Mutation signup");
-                return this.memberService.signup(input);
+                return await this.memberService.signup(input);
         }
 
         @Mutation(() => Member)
         public async login(@Args("input") input: LoginInput): Promise<Member> {
                 console.log("Mutation: login");
-                return this.memberService.login(input);
+                return await this.memberService.login(input);
         }     
         
         @UseGuards(AuthGuard)
@@ -51,38 +51,41 @@ export class MemberResolver {
             @Args('input') input: MemberUpdate,
             @AuthMember("_id") memberId: ObjectId): Promise<Member> {
             console.log("Mutation: updateMember");
-            delete input._id;  //////******************************************************************//////
-            return this.memberService.updateMember(memberId, input);
+            delete input._id;  //////***********************DELETE givinf error but still working*************************//////
+            return await this.memberService.updateMember(memberId, input);
         }
         @UseGuards(WithoutGuard)
         @Query(() => Member)
         public async getMember(@Args('memberId') input: string, @AuthMember('_id') memberId: ObjectId): Promise<Member> {
             console.log("Query: getMember");
             const targetId = shapeIntoMongoObjectId(input)
-            return this.memberService.getMember(memberId, targetId);
+            return await this.memberService.getMember(memberId, targetId);
         }
 
         @UseGuards(WithoutGuard)
         @Query(() => Members)
         public async getAgents(@Args('input') input: AgentInquery, @AuthMember('_id') memberId: ObjectId): Promise<Members> {
             console.log("Query: getAgents")
-            return this.memberService.getAgents(memberId, input);
+            return await this.memberService.getAgents(memberId, input);
         }
 
         /** ADMIN **/
 
         @Roles(MemberType.ADMIN)
         @UseGuards(RolesGuard)
-        @Mutation(() => String)
-        public async getAllMemberByAdmin(): Promise<string> {
-            return this.memberService.getAllMemberByAdmin();
+        @Query(() => Members)
+        public async getAllMemberByAdmin(@Args('input') input: MembersInquery): Promise<Members> {
+            console.log("Mutation: getAllMemberByAdmin");
+            return await this.memberService.getAllMemberByAdmin(input);
         }
 
         //Authorization: ADMIN
-        @Mutation(() => String)
-        public async updateMemberByAdmin(): Promise<string> {
+        @Roles(MemberType.ADMIN)
+        @UseGuards(RolesGuard) 
+        @Mutation(() => Member)
+        public async updateMemberByAdmin(@Args('input') input: MemberUpdate): Promise<Member> {
             console.log("Mutation: updateMemberByAdmin");
-            return this.memberService.updateMemberByAdmin();
+            return await this.memberService.updateMemberByAdmin(input);
         }
 
 }
--- apps/nestar-api/src/libs/config.ts
+++ apps/nestar-api/src/libs/config.ts
@@ -2,6 +2,8 @@ import { ObjectId } from "bson";
 
 export const availableAgentSorts = ["createdAt", "updatedAt", "memberLikes", "memberViews", "memberRank" ]
 
+export const availableMemberSorts = ["createdAt", "updatedAt", "memberLikes", "memberViews" ]
+
 export const shapeIntoMongoObjectId = (tager: any) => {
     return typeof tager === "string" ? new ObjectId(tager) : tager;
 }
