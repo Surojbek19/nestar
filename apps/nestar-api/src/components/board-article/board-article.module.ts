656c122b4bc4a6562da57e36db80b86db00ed1cd
Sam <surojbeksv@gmail.com>
2026-01-13 14:58:29 +0900

    feat: configure boardArticle module and integrate boardArticle DTO

--- apps/nestar-api/src/components/board-article/board-article.module.ts
+++ apps/nestar-api/src/components/board-article/board-article.module.ts
@@ -1,4 +1,20 @@
 import { Module } from '@nestjs/common';
+import { BoardArticleResolver } from './board-article.resolver';
+import { BoardArticleService } from './board-article.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import BoardArticleSchema from '../../schemas/BoardArticle.model';
+import { AuthModule } from '../auth/auth.module';
+import { MemberModule } from '../member/member.module';
+import { ViewModule } from '../view/view.module';
 
-@Module({})
-export class BoardArticleModule {}
+@Module({
+  imports: [
+      MongooseModule.forFeature([{name: "BoardArticle", schema: BoardArticleSchema}]), //This code for BoardArticleSchema model integration
+      AuthModule,
+      ViewModule,
+      MemberModule,
+    ],
+  providers: [BoardArticleResolver, BoardArticleService],
+  exports: [BoardArticleService] //This is a NestJS dependency-injection export, not TypeScript. “If another module imports BoardArticleModule, it can use BoardArticleService”
+})
+export class BoardArticleModule {} //This is a TypeScript export. It allows other files to import it's module
