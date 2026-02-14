10e51c41680dde61c1b73e953f76fdbb0d23d8bc
Sam <surojbeksv@gmail.com>
2026-02-15 01:57:46 +0900

    fix: modify follow.service.ts and member.update.ts logic

--- apps/nestar-api/src/libs/dto/member/member.update.ts
+++ apps/nestar-api/src/libs/dto/member/member.update.ts
@@ -11,43 +11,43 @@ export class MemberUpdate {
     _id: ObjectId;
 
     @IsOptional()
-    @Field(() => MemberType, {nullable: true})
+    @Field(() => MemberType, { nullable: true })
     memberType?: MemberType;
 
     @IsOptional()
-    @Field(() => MemberStatus, {nullable: true})
+    @Field(() => MemberStatus, { nullable: true })
     memberStatus?: MemberStatus;
 
     @IsOptional()
-    @Field(() => String, {nullable: true}) 
+    @Field(() => String, { nullable: true })
     memberPhone?: string;
 
     @IsOptional()
     @Length(3, 12)
-    @Field(() => String, {nullable: true}) 
+    @Field(() => String, { nullable: true })
     memberNick?: string;
 
     @IsOptional()
     @Length(5, 12)
-    @Field(() => String, {nullable: true}) 
+    @Field(() => String, { nullable: true })
     memberPassword?: string;
 
     @IsOptional()
     @Length(3, 100)
-    @Field(() => String, {nullable: true}) 
+    @Field(() => String, { nullable: true })
     memberFullName?: string;
 
     @IsOptional()
-    @Field(() => String, {nullable: true}) 
+    @Field(() => String, { nullable: true })
     memberImage?: string;
 
     @IsOptional()
-    @Field(() => String, {nullable: true}) 
+    @Field(() => String, { nullable: true })
     memberAddress?: string;
 
     @IsOptional()
-    @Field(() => String, {nullable: true}) 
+    @Field(() => String, { nullable: true })
     memberDesc?: string;
 
-    deleteAt?: Date;
+    deletedAt?: Date;
 }
